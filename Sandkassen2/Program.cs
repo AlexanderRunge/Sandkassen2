c1b379326ea011fea37e7bda041f3a24326011ba
ChilledMantis <alexander@khrunge.dk>
2022-05-05 08:18:45 +0200

    Add project files.

---
+++ Sandkassen2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Sandkassen2;
+
+bool test = false;
+
+UI.ShowMenu();
+do
+{
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    if (keyInfo.Key == ConsoleKey.X)
+    {
+        test = true;
+        return;
+    }
+    if (keyInfo.Key == ConsoleKey.S)
+    {
+        UI.ShowSearch();
+    }
+    if (keyInfo.Key == ConsoleKey.N)
+    {
+        UI.Welcome();
+    }
+    if (keyInfo.Key == ConsoleKey.M)
+    {
+        UI.ShowMenu();
+    }
+} while (test == false);
---
+++ Sandkassen2/UI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandkassen2
+{
+    internal static class UI
+    {
+        public static void Welcome()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.WriteLine("Når du er færdig, så tast [M] for at komme til menuen, eller tryk [X], for at forlade programmet");
+            Console.WriteLine("Hvad hedder du?");
+            string navn = Console.ReadLine();
+            UdskrivNavn(navn);
+        }
+
+        public static void ShowSearch()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            bool test2 = false;
+
+            Console.WriteLine("Når du er færdig, så tast [M] for at komme til menuen, eller tryk [X], for at forlade programmet");
+            Console.WriteLine("Hvad vil du søge: ");
+            string søg = Console.ReadLine();
+            Console.WriteLine($"Her er resultaterne for søgningen: {søg}");
+            søg = søg.ToLower();
+            if (søg == "brød")
+            {
+                Console.WriteLine("Forskellige typer brød: \n" +
+                                  "1.\t Rugbrød \n" +
+                                  "2.\t Fransbrød \n" +
+                                  "3.\t Surdejsbrød \n");
+            }
+            else
+            {
+                Console.WriteLine("Der var ingen resultater");
+            }
+        }
+
+        public static void ShowMenu()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Tast [M] for at komme til menu, [X] for at lukke programet, [S] for at søge, [N] for at give dit navn");
+        }
+
+        private static void UdskrivNavn(string navn)
+        {
+            Console.WriteLine($"Du hedder: {navn}");
+        }
+        
+    }
+}
